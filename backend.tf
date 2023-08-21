be9a2f3ce1136dcb4082bb554006250d8f809fd7
dadrake3 <dadrake3@gmail.com>
2023-08-21 15:10:45 -0500

    fixed backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "${var.prefix}-terraform-state"
+    bucket = "daifugo_api-terraform-state"
     key    = "terraform.tfstate"
-    region = "${var.region}"
+    region = "us-east-1"
   }
 }
